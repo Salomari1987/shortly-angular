410275e1b11057ff70ac3ab60786becc372957d8
emano93 <emanothman506@gmail.com>
2016-07-13 20:30:56 +0300

    update shorten and services files

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,19 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
-});
+	$scope.link = {} 
+	if(!$scope.link.url){
+		$scope.message = 'Enter a URL'
+	} else{
+		Links.create($scope.link)
+		.then(function(link) {
+			console.log(link)
+			//route to links
+		})
+		.catch(function(err){
+			console.error(err);
+			$scope.message = 'Enter a valid URL'
+		})	
+	}
+	
+ });

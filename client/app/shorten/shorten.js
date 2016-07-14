3fa0793d3c8903c406649b3241bb1e0cfd3f95ac
emano93 <emanothman506@gmail.com>
2016-07-14 11:16:36 +0300

    Configure shorten controller

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,18 +2,13 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
 	$scope.link = {} 
-	if(!$scope.link.url){
-		$scope.message = 'Enter a URL'
-	} else{
-		Links.create($scope.link)
-		.then(function(link) {
-			console.log(link)
-			//route to links
+	$scope.addLink = function(){
+		Links.addOne($scope.link)
+			.then(function(link) {
 		})
 		.catch(function(err){
-			console.error(err);
 			$scope.message = 'Enter a valid URL'
-		})	
+		})		
 	}
 	
  });
